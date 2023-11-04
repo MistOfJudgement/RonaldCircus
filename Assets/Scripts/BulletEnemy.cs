f020c62f17cb96c9a5295f72d7b8488bbffeaebf
ThermiteFe8 <thermitefe8@gmail.com>
2023-11-04 16:16:54 -0400

    aRT AND OTHER  SHIT
    oops caps was on

--- Assets/Scripts/BulletEnemy.cs
+++ Assets/Scripts/BulletEnemy.cs
@@ -14,7 +14,7 @@ public class BulletEnemy : MonoBehaviour
         rb.velocity = transform.up * speed;
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
