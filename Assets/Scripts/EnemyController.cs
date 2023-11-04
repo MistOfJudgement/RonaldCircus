b643297a9677127823ae12998d77597723bbb3dc
Tushar <stuffcopresident@gmail.com>
2023-11-04 16:15:26 -0400

    Incomplete Wave code

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,7 @@ public class EnemyController : MonoBehaviour
 {
     public EnemyHealth health;
     public IEnemyBehavior behavior;
+
     // Start is called before the first frame update
     void Start()
     {
