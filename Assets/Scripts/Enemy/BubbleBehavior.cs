5c151036a16200a5eba269418b29c985ad701418
Tushar <stuffcopresident@gmail.com>
2023-11-04 23:57:16 -0400

    Better Bubbles

---
+++ Assets/Scripts/Enemy/BubbleBehavior.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleBehavior : MonoBehaviour, IEnemyBehavior, IHitter<PlayerController>
+{
+
+    private GameObject player;
+    public Rigidbody2D rb;
+    private EnemyController storedEnemy;
+    public float acceleration = 1f;
+    public int damage = 1;
+
+    private void Start()
+    {
+        storedEnemy = transform.childCount > 0 ? transform.GetChild(0).GetComponent<EnemyController>() : null;
+        storedEnemy?.Disable();
+        storedEnemy?.transform.localPosition.Set(0, 0, 0);
+        player = GameObject.FindGameObjectWithTag("Player");
+        rb = GetComponent<Rigidbody2D>();
+        GetComponent<EnemyHealth>().OnDeath += Bubble_OnDeath;
+    }
+
+    private void Bubble_OnDeath()
+    {
+        if (storedEnemy == null)
+        {
+            return;
+        }
+        storedEnemy.Enable();
+        storedEnemy.transform.parent = null;
+        storedEnemy.transform.position = transform.position;
+    }
+
+    private void Update()
+    {
+        rb.velocity += 
+            (Vector2)(player.transform.position - 
+            transform.position).normalized * 
+            acceleration * 
+            Time.deltaTime;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<PlayerController>(out var _player))
+        {
+            DoHit(_player);
+        }
+    }
+    public void StartBehavior()
+    {
+        enabled = true;
+    }
+
+    public void StopBehavior()
+    {
+        enabled = false;
+    }
+
+    public void DoHit(PlayerController hittable)
+    {
+        hittable.TakeDamage(damage);
+        hittable.OnHit();
+    }
+}
--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -8,6 +8,7 @@ public class EnemyController : MonoBehaviour, IHittable
 {
     public EnemyHealth health;
     public IEnemyBehavior behavior;
+    private Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
@@ -16,18 +17,21 @@ public class EnemyController : MonoBehaviour, IHittable
         //TODO Jank
         behavior = GetComponent<IEnemyBehavior>();
         behavior.StartBehavior();
+        rb = GetComponent<Rigidbody2D>();
     }
     public void Disable()
     {
         health.enabled = false;
         behavior.StopBehavior();
         this.enabled = false;
+        rb.isKinematic = true;
     }
     public void Enable()
     {
         health.enabled = true;
         behavior.StartBehavior();
         this.enabled = true;
+        rb.isKinematic = false;
     }
     // Update is called once per frame
     void Update()
