52c63b6d2a2f3a29bc2ef7463bffbf0339dfe4c5
ThermiteFe8 <thermitefe8@gmail.com>
2023-11-05 01:50:53 -0500

    HLEath REOWKR
    heart.

--- Assets/Scripts/Player/PlayerHealthBarUI.cs
+++ Assets/Scripts/Player/PlayerHealthBarUI.cs
@@ -8,6 +8,9 @@ public class PlayerHealthBarUI : MonoBehaviour
 {
     public TextMeshProUGUI healthText;
     public PlayerHealth playerHealth;
+    public Sprite fullHeart;
+    public Sprite halfHeart;
+    public GameObject[] myyyyyy;
 
     private void OnEnable()
     {
@@ -23,5 +26,24 @@ public class PlayerHealthBarUI : MonoBehaviour
     public void UpdateHealthText()
     {
         healthText.text = "Health: " + playerHealth.CurrentHealth + "/" + playerHealth.maxHealth;
+        for(int i = 0; i < myyyyyy.Length; i++)
+        {
+            myyyyyy[i].GetComponent<Image>().sprite = fullHeart;
+            myyyyyy[i].SetActive(false);
+        }
+
+
+
+        for(int i = 0;i < playerHealth.CurrentHealth/2; i++)
+        {
+            myyyyyy[i].SetActive(true);
+        }
+
+        if(playerHealth.CurrentHealth%2 != 0)
+        {
+            myyyyyy[playerHealth.CurrentHealth / 2].SetActive(true);
+            myyyyyy[playerHealth.CurrentHealth/2].GetComponent<Image>().sprite = halfHeart; 
+        }
+
     }
 }
