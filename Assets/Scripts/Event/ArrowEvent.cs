4efdf7abdb614988c66477ab07b4eb26bfb9b968
Tushar <stuffcopresident@gmail.com>
2023-11-05 04:24:43 -0500

    ArrowEvent

---
+++ Assets/Scripts/Event/ArrowEvent.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowEvent : OneShotEvent
+{
+    public GameObject[] arrowPrefabs;
+    public override void FireEvent()
+    {
+        GameObject prefab = arrowPrefabs[Random.Range(0, arrowPrefabs.Length)];
+        Instantiate(prefab);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
